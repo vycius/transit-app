608cc2e72a6af2ceace16ce9c0cb7453e29dfb54
Karolis Vyčius <k.vycius@gmail.com>
2022-08-16 15:37:37 +0300

    Coordinates for cities

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,9 @@
 import 'package:latlong2/latlong.dart';
 
-final defaultLatLng = LatLng(54.68916, 25.2798);
+final defaultLatLng = _vilniusLatLng;
+
+final _vilniusLatLng = LatLng(54.68916, 25.2798);
+final _kaunasLatLng = LatLng(54.9, 23.9);
+final _klaipedaLatLng = LatLng(55.703297, 21.144279);
+final _panevezysLatLng = LatLng(55.73333, 24.35);
+final _druskininkaiLatLng = LatLng(54.01667, 23.96667);
